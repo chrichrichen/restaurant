f34c6efd1a8e54bf20769330592b28a3f15e95e2
Christal Chen <christalchen1209@gmail.com>
2022-11-17 15:36:40 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
