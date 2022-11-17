eb0991307feb2216668e1131f3a7ec30d7be92cd
Christal Chen <christalchen1209@gmail.com>
2022-11-17 17:24:05 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -14,6 +14,11 @@ router.post('/login',passport.authenticate('local',{
 
 )
 
+router.get('/logout',(req,res)=>{
+  req.logout()
+  res.redirect('/users/login')
+})
+
 router.get('/register',(req,res)=>{
   res.render('register')
 })
