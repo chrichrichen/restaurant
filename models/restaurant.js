2bee591dd002b3ffbdc744d05a85e79b975145f5
Christal Chen <christalchen1209@gmail.com>
2022-11-18 00:20:54 +0800

    feat: add relations between User and Todo models

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,3 +1,4 @@
+const { SchemaTypeOptions } = require('mongoose')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
@@ -9,6 +10,7 @@ const restaurantSchema = new Schema({
   phone: { type: String, required: true },
   google_map: { type: String, required: true },
   rating: { type: Number, required: true },
-  description: { type: String, required: true }
+  description: { type: String, required: true },
+  userId:{type: Schema.Types.ObjectId, ref:'User',index:true,required:true}
 })
 module.exports = mongoose.model('RestaurantList', restaurantSchema)
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,60 +2,58 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
+
 router.get('/new', (req, res) => {
-  return res.render('new')
+    return res.render('new')
 })
+
 
 router.post('/', (req, res) => {
-  const name = req.body.name
-  return Restaurant.create(req.body)
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    const userId = req.user._id
+    Restaurant.create({ ...req.body, userId })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
+
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
+    const _id = req.params.id
+    const userId = req.user._id
+    return Restaurant.findOne({ _id, userId })
+        .lean()
+        .then(restaurant => res.render('detail', { restaurant }))
+        .catch(error => console.log(error))
 })
 
+
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
+    const _id = req.params.id
+    const userId = req.user._id
+    return Restaurant.findOne({ _id, userId })
+        .lean()
+        .then(restaurant => res.render('edit', { restaurant }))
+        .catch(error => console.error(error))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
-  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
+    const _id = req.params.id
+    const userId = req.user._id
 
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
+
+    return Restaurant.findOneAndUpdate({ _id, userId }, req.body)
+        .then(() => res.redirect(`/restaurants/${_id}`))
+        .catch(error => console.error(error))
 })
 
+
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    const _id = req.params.id
+    const userId = req.user._id
+
+
+    return Restaurant.findOneAndDelete({ _id, userId })
+        .then(() => res.redirect('/'))
+        .catch(error => console.error(error))
 })
 
 module.exports = router
