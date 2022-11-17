c1fab4fa39eb6d7f173e2056540f6db9cd86f638
Christal Chen <christalchen1209@gmail.com>
2022-11-17 18:19:57 +0800

    feat: add user authentication in restaurant and home router

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,11 @@
 const passport = require('passport')
-const user = require('../models/user')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
 module.exports = app => {
+  // 初始化 Passport 模組
   app.use(passport.initialize())
   app.use(passport.session())
-
+  // 設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ email })
       .then(user => {
@@ -18,7 +19,6 @@ module.exports = app => {
       })
       .catch(err => done(err, false))
   }))
-
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
