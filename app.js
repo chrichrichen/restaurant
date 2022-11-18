61511c7a00b81d7a9ae168413e43bf4283456dc3
Christal Chen <christalchen1209@gmail.com>
2022-11-18 16:39:37 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 const express = require('express')
-const session = require('express-session')
-const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
+const usePassport = require('./config/passport')
+
 const routes = require('./routes')
 require('./config/mongoose')
-const PORT = process.env.PORT || 3000
+
+
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-app.use(session({
-  secret:'ThisIsMySecret',
-  resave: false,
-  saveUninitialized:true
-}))
-
 app.use(express.static('public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
 usePassport(app)
 app.use(flash())
-app.use((req,res,next)=>{
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg = req.flash('warning_msg')
-  next()
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated
+    res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.warning_msg = req.flash('warning_msg')
+    next()
 })
-
 app.use(routes)
-
-app.listen(PORT, () => {
-  console.log(`creating restaurant project http://localhost:${PORT}`)
-})
+app.listen(3000, () => {
+    console.log(`Express is running on http://localhost:3000`)
+})//接收server
